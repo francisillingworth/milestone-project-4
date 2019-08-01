b924714a6a75db8c74070435670151bdb3a9df9f
Ubuntu <ubuntu@ip-172-31-36-89.ec2.internal>
2019-08-01 13:56:28 +0000

    update urls.py

--- issues/urls.py
+++ issues/urls.py
@@ -5,5 +5,5 @@ from .views import all_issues, NewIssue
 
 urlpatterns = [
     url(r'^$', all_issues, name='issues'),
-    url(r'^issues/$', NewIssue, name='NewIssue'),
+    url(r'^NewIssue/$', NewIssue, name='NewIssue'),
 ]
