e6178390317f4ed552b46b3c94eadc392e7e2a7e
Ubuntu <ubuntu@ip-172-31-36-89.ec2.internal>
2019-08-01 13:49:50 +0000

    updated issues.urls.py

--- issues/urls.py
+++ issues/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
-from .views import all_issues
+from .views import all_issues, NewIssue
 
 
 
 urlpatterns = [
     url(r'^$', all_issues, name='issues'),
+    url(r'^issues/$', NewIssue, name='NewIssue'),
 ]
